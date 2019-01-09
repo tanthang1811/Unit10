7d3b518c6de5c95d806ba18d3508e3371f6e1bc5
Victor <nguyentanthang9a4@gmail.com>
2019-01-09 16:00:25 +0700

    Commit #1

---
+++ lesson1.js
@@ -0,0 +1,22 @@
+{
+function person(name, age, favDrink){
+  this.name = name;
+  this.age = age;
+  this.favDrink = favDrink;
+  this.changeName = function (name) {
+    this.name = name;
+  }
+  this.changeAge = ageChanging;
+}
+function ageChanging() {
+  return 2018 - this.age;
+}
+var p1 = new person("Alex", 30, "Apple Juice");
+var p2 = new person("Hagi", 23, "Pineapple Juice");
+var Max = {name : "Max", age: 18, favDrink: "Beer"};
+console.log(Max);
+console.log(p1.name);
+p1.changeName("Christian");
+console.log(p1.changeAge());
+
+}
